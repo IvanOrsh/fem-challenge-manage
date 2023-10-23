4d7ed0460694d524f94ad1024b7dfc0350926f5d
IvanOrsh <ksironov@mail.ru>
2023-10-23 13:03:19 +0300

    feat: navbar

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
+const iconHam = document.querySelector(".icon-hamburger");
+const iconClose = document.querySelector(".icon-close");
+
+navToggle.addEventListener("click", () => {
+  if (primaryNav.hasAttribute("data-visible")) {
+    navToggle.setAttribute("aria-expanded", false);
+  } else {
+    navToggle.setAttribute("aria-expanded", true);
+  }
+  primaryNav.toggleAttribute("data-visible");
+  iconClose.toggleAttribute("data-visible");
+  iconHam.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
